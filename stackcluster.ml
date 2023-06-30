ab89408f7365b8aa0c001f74ed3166b7daff6111
Koospal <stefan@koospal.de>
2023-06-30 16:14:59 +0200

    test ml

---
+++ stackcluster.ml
@@ -0,0 +1,32 @@
+# STACK-GoeMaxima Cluster as a Service
+
+The GWDG, a service provider for research institutes 
+and universities, located in Goettingen, Germany, is 
+offering scalable STACK GoeMaxima cluster as a service.
+
+## Benefits
+
+The benefits of the cluster are:
+- all backend versions available
+-- after plugin or LMS update
+--- just change the URL
+--- easy rollback
+- rolling updates
+- tested macro support
+- high availability
+- easier plugin update
+
+## Conditions
+
+- 1.7. to 31.12.2023 500 Euro
+- From 1.1.2024 per year prices depending on the size of the university/organisation.
+-- 1 to 2000 students: 1400 Euro
+-- 2000-10000 students: 1900 Euro
+-- 10000-20000 students: 2300 Euro
+-- from 20000 students: 2600 Euro
++ VAT
+
+## Contact
+
+> stack-support@gwdg.de
+https://gwdg.de
